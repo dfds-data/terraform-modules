96a708ea56119dd27c916716d5378a9971b59d16
Felipe Poulsen <fepou@dfds.com>
2021-08-31 11:26:38 +0200

    remove stuff to upload lambda function

--- modules/base_infrastructure/main.tf
+++ modules/base_infrastructure/main.tf
@@ -1,9 +1,3 @@
-locals {
-  output_path = format("%s/metrics_lambda_function_payload.zip", var.builds_bucket)
-}
-
-
-
 resource "aws_s3_bucket" "builds_bucket" {
   bucket_prefix = var.builds_bucket
   acl           = "private"
@@ -31,30 +25,4 @@ resource "aws_s3_bucket" "output_bucket" {
 
 resource "aws_glue_catalog_database" "glue_database" {
   name = var.glue_database
-}
-
-
-# Zip the Lamda function on the fly
-data "archive_file" "source" {
-  type        = "zip"
-  source_file = var.lambda_path
-  output_path = "lambda_functions/metrics_lambda_function_payload.zip"
-}
-# upload zip to s3 and then update lamda function from s3
-resource "aws_s3_bucket_object" "file_upload" {
-  bucket = "${aws_s3_bucket.builds_bucket.id}"
-  key    = "metrics_lambda_function_payload.zip"
-  source = "${data.archive_file.source.output_path}" # its mean it depended on zip
-}
-
-
-locals {
-  my_function_source = "../path/to/package.zip"
-}
-
-
-resource "aws_s3_bucket_object" "my_function" {
-  bucket = aws_s3_bucket.builds_bucket.id
-  key    = "${filemd5(local.my_function_source)}.zip"
-  source = local.my_function_source
 }
