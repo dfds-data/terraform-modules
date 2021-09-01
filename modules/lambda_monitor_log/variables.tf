6b866f6e98eb27f3f9096334c351333e6bf462dd
Felipe Poulsen <fepou@dfds.com>
2021-09-01 12:55:01 +0200

    add variables

--- modules/lambda_monitor_log/variables.tf
+++ modules/lambda_monitor_log/variables.tf
@@ -33,3 +33,26 @@ variable "role_policies" {
   type        = list(string)
   default     = ["arn:aws:iam::aws:policy/CloudWatchLogsFullAccess", "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"]
 }
+
+variable "lambda_runtime" {
+  description = "Python runtime"
+  type        = string
+  default = "python3.8"
+}
+
+variable "cloudwatch_retention_days" {
+  type        = number
+  default = 14
+}
+
+variable "memory_size" {
+  description = "Amount of memory in MB your Lambda Function can use at runtime"
+  type        = number
+  default     = 128
+}
+
+variable "timeout" {
+  description = "Amount of time your Lambda Function has to run in seconds."
+  type        = number
+  default     = 12
+}
