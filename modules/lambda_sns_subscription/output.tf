b44204710524ac184eca7934b2a2db614c54ae19
Felipe Poulsen <fepou@dfds.com>
2021-09-09 12:34:17 +0200

    put resource name in output

--- modules/lambda_sns_subscription/output.tf
+++ modules/lambda_sns_subscription/output.tf
@@ -3,11 +3,12 @@ output "lambda_function_arn" {
   value       = aws_lambda_function.lambda_function.arn
 }
 
-output "lambda_function_name" {
-  description = "ARN of the lambda function"
-  value       = local.resource_name
-}
 output "iam_role_name" {
   description = "Name of the IAM role"
   value       = aws_iam_role.instance.name
+}
+
+output "resource_name" {
+  description = "Resource name"
+  value       = local.resource_name
 }
