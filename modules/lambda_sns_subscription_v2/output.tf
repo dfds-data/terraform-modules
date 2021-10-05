5736b0e61027c49cbe9f94c09151510cef13c4d2
Felipe Poulsen <fepou@dfds.com>
2021-10-05 12:53:00 +0200

    fix output variables

--- modules/lambda_sns_subscription_v2/output.tf
+++ modules/lambda_sns_subscription_v2/output.tf
@@ -1,21 +1,21 @@
 output "lambda_function_arn" {
   description = "ARN of the lambda function"
-  value       = aws_lambda_function.lambda_function.arn
+  value       = module.lambda_base.lambda_function_arn
 }
 
 output "iam_role_name" {
   description = "Name of the IAM role"
-  value       = aws_iam_role.instance.name
+  value       = module.lambda_base.iam_role_name
 }
 
 output "resource_name" {
   description = "Resource name"
-  value       = local.resource_name
+  value       = module.lambda_base.resource_name
 }
 
 output "log_group_name" {
   description = "Name of the AWS Cloudwatch log group"
-  value       = aws_cloudwatch_log_group.log_lambda.name
+  value       = module.lambda_base.log_group_name
 }
 
 output "topic_arn" {
