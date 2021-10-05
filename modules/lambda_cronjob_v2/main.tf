0ae504bcaaaf42090904ae20268f48a0a0626415
Felipe Poulsen <fepou@dfds.com>
2021-10-05 12:49:03 +0200

    remove variables from monitor

--- modules/lambda_cronjob_v2/main.tf
+++ modules/lambda_cronjob_v2/main.tf
@@ -41,10 +41,6 @@ module "monitor" {
   entity_name   = format("mntr-%s", var.entity_name)
   log_group     = module.lambda_base.log_group_name
   image_uri = "469457075771.dkr.ecr.eu-central-1.amazonaws.com/send-message-to-teams:latest"
-  package_type = "Image"
-  lambda_runtime = null
-  lambda_handler = null
-  lambda_function_payload_key = null
 
   environment_variables = {
       webhook_url = "https://dfds.webhook.office.com/webhookb2/099086f9-7359-4d9c-a0f1-d6e48882f42a@73a99466-ad05-4221-9f90-e7142aa2f6c1/IncomingWebhook/bd7eb83cd19440b8b2928ccaecb9d5f2/d7bb3513-a242-4d53-9959-807f4ececf3a"
